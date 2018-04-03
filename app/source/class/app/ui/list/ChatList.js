7b26f46307770a7d2117beb574f4c0f41bddaa4d
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-04-03 20:38:54 +0200

    add license header to source files

--- app/source/class/app/ui/list/ChatList.js
+++ app/source/class/app/ui/list/ChatList.js
@@ -1,3 +1,22 @@
+/* DeerNation community project
+ *
+ * copyright (c) 2017-2018, Tobias Braeutigam.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
 /**
  * ChatList is a virtual list that provided an autoscrolling feature to always show the lasz list item.
  * This behaviour is active when the user scrolls to the bottom of the list (which is done automatically on initial loading)
