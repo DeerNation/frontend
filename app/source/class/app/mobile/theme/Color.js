ce09a0e9b238afe5eb2336d454f42937e9c47626
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-04-03 20:32:33 +0200

    change group line color

--- app/source/class/app/mobile/theme/Color.js
+++ app/source/class/app/mobile/theme/Color.js
@@ -1,12 +1,3 @@
-/* ************************************************************************
-
-   Copyright:
-
-   License:
-
-   Authors:
-
-************************************************************************ */
 
 qx.Theme.define('app.mobile.theme.Color', {
   extend: app.theme.Color,
@@ -14,6 +5,7 @@ qx.Theme.define('app.mobile.theme.Color', {
   colors: {
     'button-left-border': 'rgba(255, 255, 255, 0.5)',
     'button-right-border': 'rgba(0, 0, 0, 0.5)',
-    'button-border': 'rgba(158, 162, 168, 0.2)'
+    'button-border': 'rgba(158, 162, 168, 0.2)',
+    'activity-group-line': 'rgba(100, 100, 100, 0.2)'
   }
 })
